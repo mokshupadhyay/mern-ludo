a6a2d84c30acd117dd36a930739ace7adc037261
Moksh Upadhyay <mokshupadhyay4@gmail.com>
2025-09-27 22:15:58 +0530

    updated player Schema and added scoring methods

--- backend/models/player.js
+++ backend/models/player.js
@@ -8,6 +8,9 @@ const PlayerSchema = new Schema({
     color: String,
     ready: { type: Boolean, default: false },
     nowMoving: { type: Boolean, default: false },
+    score: { type: Number, default: 0 },
+    pawnsInHome: { type: Number, default: 0 },
+    pawnsCaptured: { type: Number, default: 0 },
 });
 
 PlayerSchema.methods.changeReadyStatus = function () {
@@ -20,6 +23,29 @@ PlayerSchema.methods.canMove = function (room, rolledNumber) {
         if (pawn.canMove(rolledNumber)) return true;
     }
     return false;
+};
+
+// Scoring methods
+PlayerSchema.methods.addProgressScore = function (steps) {
+    // 1 point per step moved
+    this.score += steps;
+};
+
+PlayerSchema.methods.addCaptureScore = function () {
+    // 10 points for capturing an opponent's pawn
+    this.score += 10;
+    this.pawnsCaptured += 1;
+};
+
+PlayerSchema.methods.addHomeScore = function () {
+    // 50 points for getting a pawn home
+    this.score += 50;
+    this.pawnsInHome += 1;
+};
+
+PlayerSchema.methods.addWinBonus = function () {
+    // 100 points bonus for winning the game
+    this.score += 100;
 };
 
 module.exports = PlayerSchema;
